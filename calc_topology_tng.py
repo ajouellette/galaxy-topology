882d5c3c47ed4b8e26f4515bfdb0d5302ac61a5f
Aaron Ouellette <aaronjo2@illinois.edu>
2022-11-09 11:31:56 -0600

    rework TNG analysis to use FoF catalog for halos

--- calc_topology_tng.py
+++ calc_topology_tng.py
@@ -4,13 +4,15 @@ import numpy as np
 from scipy.interpolate import interp1d
 import gudhi as gd
 import gudhi.representations as gdr
+from Corrfunc.theory.xi import xi
+import powerbox as pb
 from alpha_complex_periodic import calc_persistence
 import illustris_python as tng
 
 
 # selection cuts
-n_star_cut = 50
-mass_cut = 3e8
+gal_mass_cut = 2e8
+halo_mass_cut = 1e10
 ssfr_cut = 10**-10.5
 match_ssfr = True
 
@@ -20,15 +22,10 @@ selections = ["galaxy", "halo", "sfgal", "qsntgal"]
 
 def main():
 
-    if len(sys.argv) < 2:
-        print("Must provide simulation name and object selection")
+    if len(sys.argv) < 1:
+        print("Must provide simulation name")
         sys.exit(1)
     sim = sys.argv[1]
-    selection = sys.argv[2]
-
-    if selection not in selections:
-        print("selection must be one of", selections)
-        sys.exit(1)
 
     # z=0 snapshot
     if "TNG" in sim:
@@ -39,7 +36,7 @@ def main():
         print("Unknown simulation")
         sys.exit()
 
-    subhalo_fields = ["SubhaloPos", "SubhaloMassType", "SubhaloLenType", "SubhaloSFR"] \
+    subhalo_fields = ["SubhaloPos", "SubhaloMassType", "SubhaloSFR"] \
             + ["SubhaloFlag"] if "Illustris" not in sim else []
     group_fields = ["GroupPos", "Group_M_Crit200"]
 
@@ -61,26 +58,19 @@ def main():
     print("Loading simulation data")
 
     subhalo_data = tng.groupcat.loadSubhalos(sim, snap_num, fields=subhalo_fields)
-    group_data = tng.groupcat.loadGroups(sim, snap_num, fields=group_fields)
+    halo_data = tng.groupcat.loadHalos(sim, snap_num, fields=group_fields)
 
-    pos = data["SubhaloPos"] / 1e3
-    dm_mass = data["SubhaloMassType"][:,1] * 1e10
-    st_mass = data["SubhaloMassType"][:,4] * 1e10
-    tot_mass = np.sum(data["SubhaloMassType"], axis=1) * 1e10
+    sh_dm_mass = subhalo_data["SubhaloMassType"][:,1] * 1e10
+    sh_st_mass = subhalo_data["SubhaloMassType"][:,4] * 1e10
+    sh_tot_mass = np.sum(subhalo_data["SubhaloMassType"], axis=1) * 1e10
     if "Illustris" not in sim:
-        flag = data["SubhaloFlag"].astype(bool)
+        flag = subhalo_data["SubhaloFlag"].astype(bool)
+        print(np.sum(~flag[(sh_st_mass > gal_mass_cut) * (sh_dm_mass / sh_tot_mass > 0.1)]))
     else:
-        flag = dm_mass / tot_mass > 0.1
-    n_stars = data["SubhaloLenType"][:,4]
+        flag = sh_dm_mass / sh_tot_mass > 0.1
 
-    print(f"min n_star with mass cut: {np.min(n_stars[(st_mass > mass_cut) * flag])}")
-    print(f"min mass with n_star cut: {np.min(st_mass[(n_stars > n_star_cut) * flag]):.3e}")
-
-    galaxy = (n_stars > n_star_cut) * (st_mass > mass_cut) * flag
-    ssfr = data["SubhaloSFR"][galaxy] / st_mass[galaxy]
-
-    # use abundance matching to get halos
-    halo = np.isin(np.arange(len(pos)), np.argsort(dm_mass)[::-1][:np.sum(galaxy)])
+    galaxy = (sh_st_mass > gal_mass_cut) * flag
+    ssfr = subhalo_data["SubhaloSFR"][galaxy] / sh_st_mass[galaxy]
 
     if not match_ssfr:
         sf = ssfr > ssfr_cut
@@ -88,38 +78,57 @@ def main():
     else:
         qsnt = np.argsort(ssfr)[:len(ssfr)//2]
         sf = np.argsort(ssfr)[len(ssfr)//2:]
+        print(f"sSFR cut: {np.max(ssfr[qsnt]):.3e}")
 
-    print(f"sSFR cut: {np.max(ssfr[qsnt]):.3e}")
+    pos_gal = subhalo_data["SubhaloPos"][galaxy] / 1e3
+    pos_sfgal = pos_gal[sf]
+    pos_qsntgal = pos_gal[qsnt]
 
-    if selection == "galaxy":
-        pos = pos[galaxy]
-    elif selection == "halo":
-        pos = pos[halo]
-    elif selection == "sfgal":
-        pos = pos[galaxy][sf]
-    elif selection == "qsntgal":
-        pos = pos[galaxy][qsnt]
+    # use abundance matching to get halos
+    m_c200 = halo_data["Group_M_Crit200"] * 1e10
+    halo = np.isin(np.arange(len(m_c200)), np.argsort(m_c200)[::-1][:len(pos_gal)])
+    pos_halo = halo_data["GroupPos"][halo] / 1e3
 
-    n_selection = len(pos)
-    print(n_selection, "objects selected")
-    lbar = boxsize / np.cbrt(n_selection)
+    n_selections = []
+    lbars = []
+    es_all = []
+    es_scaled_all = []
+    xi_all = []
+    for i, pos in enumerate([pos_gal, pos_halo, pos_sfgal, pos_qsntgal]):
+        print("Starting", selections[i])
+        n_selections.append(len(pos))
+        print(len(pos), "objects selected")
+        lbar = boxsize / np.cbrt(len(pos))
+        lbars.append(lbar)
 
-    pairs = calc_persistence(pos, boxsize=boxsize, precision="fast")
-    pairs = [np.array(pairs[d]) for d in range(3)]
+        print("Computing 2pcf..")
+        rbins = np.logspace(np.log10(lbar/2), np.log10(boxsize/3.1), 35)
+        sample_xi = xi(boxsize, 2, rbins, *pos.T, output_ravg=True)
+        xi_all.append(sample_xi)
 
-    print("Calculating ES curves")
-    es = ES.fit_transform(DS.fit_transform(pairs))
+        print("Computing topology...")
+        pairs = calc_persistence(pos, boxsize=boxsize, precision="fast")
+        pairs = [np.array(pairs[d]) for d in range(3)]
 
-    print("Scaling and interpolating ES curves")
-    interp = interp1d(alpha/lbar, es, axis=-1, bounds_error=False, fill_value=0, assume_sorted=True, kind="slinear")
-    es_scaled = interp(alpha_scaled)
-    norm = np.trapz(np.abs(es_scaled), alpha_scaled, axis=-1)
-    es_scaled /= np.expand_dims(norm, -1)
+        print("Calculating ES curves")
+        es = ES.fit_transform(DS.fit_transform(pairs))
+        es_all.append(es)
+
+        print("Scaling and interpolating ES curves")
+        interp = interp1d(alpha/lbar, es, axis=-1, bounds_error=False, fill_value=0, assume_sorted=True, kind="slinear")
+        es_scaled = interp(alpha_scaled)
+        norm = np.trapz(np.abs(es_scaled), alpha_scaled, axis=-1)
+        es_scaled /= np.expand_dims(norm, -1)
+        es_scaled_all.append(es_scaled)
 
     sim_name = sim.split('/')[-1]
-    fname = f"topology_summaries/{sim_name}/es_{selection}.npz"
+    fname = f"topology_summaries/{sim_name}/es_all.npz"
     print(f"Saving data to {fname}")
-    np.savez(fname, alpha=alpha, es=es, alpha_scaled=alpha_scaled, es_scaled=es_scaled, n_selection=n_selection, lbar=lbar)
+    np.savez(fname, alpha=alpha, alpha_scaled=alpha_scaled, n_selections=n_selections, lbars=lbars,
+            xi_gal=xi_all[0], xi_halo=xi_all[1], xi_sfgal=xi_all[2], xi_qsntgal=xi_all[3],
+            es_gal=es_all[0], es_scaled_gal=es_scaled_all[0], es_halo=es_all[1], es_scaled_halo=es_scaled_all[1],
+            es_sfgal=es_all[2], es_scaled_sfgal=es_scaled_all[2], es_qsntgal=es_all[3],
+            es_scaled_qsntgal=es_scaled_all[3])
 
 
 main()
